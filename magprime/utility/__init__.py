74207c773c6cecb5729481b1bee1b843f4054ea9
skvastro <skv.astro@gmail.com>
2024-06-04 15:40:41 +0530

    Update __init__.py

--- magprime/utility/__init__.py
+++ magprime/utility/__init__.py
@@ -1,2 +1,2 @@
-from .data_loader import load_michibiki_data, load_swarm_data
+from .data_loader import load_michibiki_data, load_swarm_data, load_quadmag_data
 from .interpolation import mssa, linear, zero_fill
